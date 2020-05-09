83437349bf9a93560f218db40a9f9640de97c698
Hitoshi Yamamoto <60470969+hitoshyamamoto@users.noreply.github.com>
2020-05-09 10:55:02 -0300

    Adicionado Solução do Problema 03

--- cplusplus/02-EvenFibonacciNumbers.cpp
+++ cplusplus/02-EvenFibonacciNumbers.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 double funcInversaBinet(double Fn){
     double cond_Fibonacci = 0.0;
-    cond_Fibonacci = sqrt(5*Fn*Fn - 4);
+    cond_Fibonacci = sqrt(5*Fn*Fn + 4);
     
     double pos_N = 0.0;
     pos_N = log((Fn*sqrt(5) + cond_Fibonacci)/2) / log(phi);
@@ -20,24 +20,18 @@ double formulaSerieGeometrica(double pos_N){
     double phi3 = phi*phi*phi;
     double psi3 = psi*psi*psi;
 
-    printf("%d || %d\n", isnan(phi3),isnan(psi3));
-
-    double somatoria_Phi = phi3*((1-pow(phi3,floor(pos_N/3)))/(1-phi3));
-    double somatoria_Psi = psi3*((1-pow(psi3,floor(pos_N/3)))/(1-psi3));
-    printf("%lf || %lf\n", somatoria_Phi, somatoria_Psi);
-    printf("%d || %d\n", isnan(somatoria_Phi),isnan(somatoria_Psi));
+    double somatoria_Phi = phi3*((1-pow(phi,floor(pos_N)))/(1-phi3));
+    double somatoria_Psi = psi3*((1-pow(psi,floor(pos_N)))/(1-psi3));
 
     double resposta_Somatoria = 0.0;
     resposta_Somatoria = (somatoria_Phi - somatoria_Psi)/sqrt(5);
-
-    printf("%d\n", isnan(resposta_Somatoria));
 
     return resposta_Somatoria;
 }
 
 int main(){
     double resposta = 0.0;
-    double valor_entrada 0.0;
+    double valor_entrada = 0.0;
     scanf("%lf",&valor_entrada);
     
     resposta = funcInversaBinet(valor_entrada);
---
+++ cplusplus/03-LargestPrimeFactor.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <math.h>
+
+using namespace std;
+
+bool quadratico(int n){
+    double raiz = sqrt(n);
+    if((raiz - floor(raiz))!=0) return true;
+    else return false;
+    
+}
+
+void crivoAtkin(unsigned long long int entrada){
+
+    //Teste com 2 e 3 que são primos
+    if(entrada%2==0){
+        printf("[2] = 2\n");
+        entrada = entrada/2;
+    }
+    if(entrada%3==0){
+        printf("[3] = 3\n");
+        entrada = entrada/3; 
+    }
+    if(entrada!=1){
+        for (int x = 1; x*x < entrada; x++) { 
+            for (int y = 1; y*y < entrada; y++) { 
+                
+                // Principal do Crivo de Atkin 
+                int n = (4*x*x) + (y*y); 
+                if (n <= entrada && (n % 12 == 1 || n % 12 == 5)){
+                    if(entrada%n==0 && quadratico(n)){
+                        printf("[%d %d] = %d\n",x,y,n);
+                        entrada = entrada/n; 
+                    }
+                }
+                n = (3*x*x) + (y*y); 
+                if (n <= entrada && n % 12 == 7){
+                    if(entrada%n==0 && quadratico(n)){
+                        printf("[%d %d] = %d\n",x,y,n);
+                        entrada = entrada/n;
+                    }
+                }
+                n = (3*x*x) - (y*y); 
+                if (x > y && n <= entrada && n % 12 == 11){
+                    if(entrada%n==0 && quadratico(n)){
+                        printf("[%d %d] = %d\n",x,y,n);
+                        entrada = entrada/n;
+                    }
+                }
+            } 
+        }
+    }
+}
+
+int main(){
+    unsigned long long int entrada = 600851475143;
+
+    crivoAtkin(entrada);
+
+    return 0;
+}
